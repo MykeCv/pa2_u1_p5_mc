61f01e66782a1be69f12ce086f62dd2fd7d5e364
MykeCv <mykecevallos@gmail.com>
2023-11-10 19:30:36 -0500

    Cambio a nueva rama taller 3

--- src/main/java/com/uce/edu/Pa2U1P5McApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5McApplication.java
@@ -17,6 +17,7 @@ public class Pa2U1P5McApplication implements CommandLineRunner{
 		System.out.println("Hola mundo");
 		System.out.println("Soy Michael Cevallos");
 		System.out.println("Cambio en la misma rama");
+		System.out.println("Cambio en una nueva rama taller 3");
 		
 	}
 
