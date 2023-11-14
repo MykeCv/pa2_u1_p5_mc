e90f5efb7c5f708bec503f14a4d6d70b32c23512
MykeCv <mykecevallos@gmail.com>
2023-11-13 19:42:34 -0500

    taller 5

--- src/main/java/com/uce/edu/Pa2U1P5McApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5McApplication.java
@@ -1,24 +1,31 @@
 package com.uce.edu;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.ioc.di.Estudiante;
+
 @SpringBootApplication
 public class Pa2U1P5McApplication implements CommandLineRunner{
+	
+	@Autowired
+	private Estudiante estudiante;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5McApplication.class, args);
-		
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo");
-		System.out.println("Soy Michael Cevallos");
-		System.out.println("Cambio en la misma rama");
-		System.out.println("Cambio en una nueva rama taller 3");
-		System.out.println("Cambio en taller 4 version 2");
+		this.estudiante.setNombre("Michael");
+		this.estudiante.setApellido("Cevallos");
+		this.estudiante.setCedula("1724362106");
+		this.estudiante.setSalario(new BigDecimal(100));
+		System.out.println(estudiante);
 	}
 
 }
